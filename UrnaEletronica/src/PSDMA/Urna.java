3494c0a31fee3130d064476fc94ce1a824b1ab15
Jhonatan Isaias <jhonatan.santos13@academico.ifs.edu.br>
2022-10-27 20:07:20 -0300

    urna finalizada

--- UrnaEletronica/src/PSDMA/Urna.java
+++ UrnaEletronica/src/PSDMA/Urna.java
@@ -18,8 +18,8 @@ public class Urna {
 		Eleitor e3 = new Eleitor("marcos","98745632","estancia");
 		Eleitor e4 = new Eleitor(" isaias dos santos","976433654","lagarto");
 		Candidato c1 = new Candidato("seu Zé","PSDSAB",1414,'P');
-		Candidato c2 = new Candidato("Senhor carlos","PSDSA",1515,'P');
-		Candidato c3 = new Candidato("Senhor carlos","PSDSA",1616,'P');
+		Candidato c2 = new Candidato("Senhor carlos","PSDSA",1515,'G');
+		Candidato c3 = new Candidato("Senhor carlos","PSDSA",1616,'S');
 		eleitores.add(e2);
 		eleitores.add(e3);
 		eleitores.add(e4);
@@ -28,24 +28,45 @@ public class Urna {
 		candidato.add(c3);
 		 
 	//inserirVotos("98745632",1616);
-	inserirVotos("9545796",1515);
-	inserirVotos("976433654",1414);
-	exibirResultado();
+	inserirVotos("9545796",1515,1414,1616);
+	resultado();
 	}
 	//**** para inserir um voto é necessário passar como parametro o RG do eleitor e o numero do candidato***
-	public static void inserirVotos(String RG ,int numCandidato) {
+	public static void inserirVotos(String RG ,int numPresidente, int numGovernador, int numSenador) {
 		
 		Eleitor mentiroso = new Eleitor("nome","rg","local");
 		eleitores.add(mentiroso);
 		
 	if(validarEleitor(RG) != null) {
-		if(validarCandidato(numCandidato) != null) {
-			Voto v = new Voto(validarCandidato(numCandidato));
+		if(validarCandidato(numPresidente) != null) {
+			Voto v = new Voto(validarCandidato(numPresidente));
 			voto.add(v);
 			System.out.println("voto cadastrado com sucesso");
-			if(validarCandidato(numCandidato).getPartido().equals("PSDSA")) {
+			if(validarCandidato(numPresidente).getPartido().equals("PSDSA")) {
 				
-				Voto Vespecial = new Voto(validarCandidato(numCandidato));
+				Voto Vespecial = new Voto(validarCandidato(numPresidente));
+				voto.add(Vespecial);
+			
+			}
+		}
+		if(validarCandidato(numGovernador) != null) {
+			Voto v = new Voto(validarCandidato(numGovernador));
+			voto.add(v);
+			System.out.println("voto cadastrado com sucesso");
+			if(validarCandidato(numGovernador).getPartido().equals("PSDSA")) {
+				
+				Voto Vespecial = new Voto(validarCandidato(numGovernador));
+				voto.add(Vespecial);
+			
+			}
+		}
+		if(validarCandidato(numSenador) != null) {
+			Voto v = new Voto(validarCandidato(numSenador));
+			voto.add(v);
+			System.out.println("voto cadastrado com sucesso");
+			if(validarCandidato(numSenador).getPartido().equals("PSDSA")) {
+				
+				Voto Vespecial = new Voto(validarCandidato(numSenador));
 				voto.add(Vespecial);
 			
 			}
@@ -70,34 +91,25 @@ public class Urna {
 		}
 		return null;
 	}
-	
-	public static void exibirResultado() {
-		 ArrayList<Voto> votoContado = new ArrayList<Voto>();
-		 boolean found ;
-		 
-		 int counter = 0;
-		for(Voto v :voto) {
-			found = false;
-			
-			
-			for(Voto v2 :votoContado){
-		       
-		        if (v == v2){
-		          counter ++;
-		          found = true;
-		          break;
-		        }
-		        System.out.println(v.getCandidato().getNome() + " recebeu "+ counter+ " votos");
-			}
-			
-			if(!found) {
-				votoContado.add(v);
-			}
-			
-			
-		}
-	
-	}
-	
 
+	
+	private static int contarVoto(Candidato candidato){
+        int total = 0;
+        for(Voto voto: voto){
+            Candidato candidato1 = voto.getCandidato();
+            if(candidato1 == null)
+                continue;
+            if(candidato1.getNumero() == candidato.getNumero()){
+                total++;
+            }
+        }
+        return total;
+    }
+	 public static void resultado(){
+	        
+	        for(Candidato candidato: candidato){
+	            System.out.println(candidato.getNome()+"("+candidato.getCargo()+"): " +
+	                    ""+contarVoto(candidato));
+	        }
+	    }
 }
